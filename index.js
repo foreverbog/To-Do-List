5509975fce239eba378ab5038b95893bce6108d2
Anagha Chadayan <anagha.chadayan@gmail.com>
2024-03-14 14:59:01 +0100

    init

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const todoTemplate = (todoTask) => `
     <input type="checkbox" id="check" name="check" class="p-2 mr-2 ml-2 w-6 h-6 rounded-full shadow-inner transition-all duration-300 ease-in-out checked:shadow-xl peer bg-slate-400/20 text-sky-500 checked:bg-sky-500 hover:bg-sky-500/30 focus:ring-sky-500"/>
     <input type="text" class="w-full h-10 font-bold leading-loose text-left bg-transparent rounded-xl border-none transition-all duration-300 ease-in-out hover:shadow-inner hover:cursor-pointer focus:ring-0 hover:border-1 hover:bg-slate-400/30 focus:bg-slate400/30 active:bg-slate-400/30" value="${todoTask}" text="text"></input>
 
-  <button type="" class="py-2 px-4 m-2 text-sm font-bold rounded-xl border-t border-white shadow-md transition-all duration-300 ease-in-out hover:shadow-xl hover:cursor-pointer text-slate-700 bg-slate-50/80 hover:text-slate-900 hover:bg-sky-500" id="deletebtn">Delete<button/>
+  <button type="" class="py-2 px-4 m-2 text-sm font-bold rounded-xl border-t border-white shadow-md transition-all duration-300 ease-in-out hover:shadow-xl hover:cursor-pointer text-slate-700 bg-slate-50/80 hover:text-slate-900 hover:bg-sky-500" id="deletebtn">Delete</button>
 </div>
 </div>
 `;
@@ -16,15 +16,16 @@ const newTodo = () => {
     alert("Item is missing");
     return true;
   }
-
+  let index = localStorage.length+1;
+  // Append a new key-value pair
+  localStorage.setItem(index,todoInput.value);
   list.insertAdjacentHTML("beforeend", todoTemplate(todoInput.value));
-  // list.lastElementChild.querySelector('#deletebtn').addEventListener('click', deleteTodo)
-  // todoInput.value = ""
+  list.lastElementChild.querySelector('#deletebtn').addEventListener('click', deleteTodo);
+  todoInput.value = "";
 };
 
-// const deleteTodo = (el) => {
-//   const todoContainer = el.target.parentElement
+const deleteTodo = (el) => {
+  const todoContainer = el.target.parentElement;
+  todoContainer.parentNode.removeChild(el.target.parentElement);
+}
 
-//   todoContainer.parentNode.removeChild(el.target.parentElement);
-
-//        }
